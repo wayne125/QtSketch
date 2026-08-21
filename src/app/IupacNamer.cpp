ba129b49a7a141b4d30b5472208563f9945b3fec
wayne125 <wayne125@users.noreply.github.com>
2026-08-21 08:06:20 +0530

    Wire in phenol as the retained PIN for hydroxybenzene

--- src/app/IupacNamer.cpp
+++ src/app/IupacNamer.cpp
@@ -9252,14 +9252,18 @@ IupacResult IupacNamer::generateName(int mol) {
                     break;
                 }
             }
-            QString stem = rootStr;
-            if (stem.endsWith("e") && !sfx.isEmpty() && isVowel(sfx[0])) stem.chop(1);
-            if (pCount == 1 && prefixPart.isEmpty() && allCarbon) {
-                fullName = prefixPart + stem + sfx;
+            if (rType == RingType::BENZENE && winningType == GroupType::ALCOHOL && pCount == 1) {
+                fullName = prefixPart + "phenol";
             } else {
-                QStringList lStrs;
-                for (int l : bestSig.principalLocants) lStrs.append(QString::number(l));
-                fullName = prefixPart + stem + QString("-%1-%2").arg(lStrs.join(","), sfx);
+                QString stem = rootStr;
+                if (stem.endsWith("e") && !sfx.isEmpty() && isVowel(sfx[0])) stem.chop(1);
+                if (pCount == 1 && prefixPart.isEmpty() && allCarbon) {
+                    fullName = prefixPart + stem + sfx;
+                } else {
+                    QStringList lStrs;
+                    for (int l : bestSig.principalLocants) lStrs.append(QString::number(l));
+                    fullName = prefixPart + stem + QString("-%1-%2").arg(lStrs.join(","), sfx);
+                }
             }
         }
     }
